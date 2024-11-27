cb94ddc9ac61c3dfde78e75f0b7a89bbc8f125a8
ssss-1b <suzanne.biju@ontariotechu.net>
2024-11-27 16:42:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('process.env.MONGO_URI',{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
